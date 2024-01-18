980f9ccf51c3524ee521726d15eeb65de7e8e7b7
Good Friend Job <somrosomro911@gmail.com>
2024-01-18 11:16:03 +0900

    installer page error

--- app/Http/Controllers/Website/WebsiteController.php
+++ app/Http/Controllers/Website/WebsiteController.php
@@ -54,9 +54,9 @@ class WebsiteController extends Controller
 {
     public function index()
     {
-        if (env('DB_DATABASE') == "") {
-            return view("first_page");
-        }
+        // if (env('DB_DATABASE') == "") {
+        //     return view("first_page");
+        // }
         $banners = Banner::get();
         $doctors = Doctor::with('category:id,name')->where([['status', 1], ['is_filled', 1], ['subscription_status', 1]])->get()->take(8);
         $treatments = Treatments::whereStatus(1)->paginate(6);
